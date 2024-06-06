e29ba5a68e0dd0cc6431aa631355fc7075237d19
MerveÇalışkan <mervecaliiskannk@gmail.com>
2024-06-06 16:06:32 +0300

    Components
    The components have been connected to the ViewController.

--- Sayi Tahmin Oyunu/Sayi Tahmin Oyunu/ViewController.swift
+++ Sayi Tahmin Oyunu/Sayi Tahmin Oyunu/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtTahminEdilecekSayi: UITextField!
+    @IBOutlet weak var imgKaydet: UIImageView!
+    @IBOutlet weak var btnKaydet: UIButton!
+    @IBOutlet weak var txtTahminSayisi: UITextField!
+    @IBOutlet weak var imgTahminDurum: UIImageView!
+    @IBOutlet weak var btnDene: UIButton!
+    @IBOutlet weak var lblSonuc: UILabel!
+    @IBOutlet weak var imgYildiz1: UIImageView!
+    @IBOutlet weak var imgYildiz2: UIImageView!
+    @IBOutlet weak var imgYildiz3: UIImageView!
+    @IBOutlet weak var imgYildiz4: UIImageView!
+    @IBOutlet weak var imgYildiz5: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnKaydetClicked(_ sender: UIButton) {
+    }
+    @IBAction func btnDeneClicked(_ sender: UIButton) {
+    }
 
 }
 
